d02cefbe08e068ebac90205935d804948e233075
Louis Bailleau <louis.bailleau@free.fr>
2024-03-14 13:56:38 +0100

    feat: move to http client instead of hardcoded values

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,65 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../types/products';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private products: Product[] = [
-    {
-      ref: '1',
-      libelle: 'Chaussures',
-      prix: 50,
-    },
-    {
-      ref: '2',
-      libelle: 'T-shirt',
-      prix: 20,
-    },
-    {
-      ref: '3',
-      libelle: 'Pantalon',
-      prix: 40,
-    },
-    {
-      ref: '4',
-      libelle: 'Veste',
-      prix: 60,
-    },
-    {
-      ref: '5',
-      libelle: 'Chaussettes',
-      prix: 10,
-    },
-    {
-      ref: '6',
-      libelle: 'Casquette',
-      prix: 15,
-    },
-    {
-      ref: '7',
-      libelle: 'Echarpe',
-      prix: 25,
-    },
-    {
-      ref: '8',
-      libelle: 'Gants',
-      prix: 20,
-    },
-    {
-      ref: '9',
-      libelle: 'Pull',
-      prix: 45,
-    },
-    {
-      ref: '10',
-      libelle: 'Jupe',
-      prix: 35,
-    },
-  ];
+  public constructor(private httpClient: HttpClient) { }
 
   public getCatalogue(): Observable<Product[]> {
-    return from([this.products]);
+    return this.httpClient.get<Product[]>('/assets/products.json')
   }
 }
