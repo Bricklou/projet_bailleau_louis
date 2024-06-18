134e1524e7d6b22fd5ee93c9cc0ceae8b3450200
Louis Bailleau <louis13.bailleau@gmail.com>
2024-06-18 23:27:51 +0200

    rework: remove refresh token handling on frontend

--- frontend/src/app/services/token.service.ts
+++ frontend/src/app/services/token.service.ts
@@ -1,21 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { TokenStorageService } from './token-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
   private accessToken = new BehaviorSubject<string | undefined>(undefined);
-  private refreshToken = new BehaviorSubject<string | undefined>(undefined);
-
-  public constructor(private tokenStorage: TokenStorageService) {
-    const refreshToken = tokenStorage.load();
-    if (refreshToken) this.refreshToken.next(refreshToken);
-  }
 
   public clearToken() {
     this.accessToken.next(undefined);
-    this.tokenStorage.deleteTokens();
   }
 }
